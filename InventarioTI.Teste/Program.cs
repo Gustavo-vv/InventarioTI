391e14f7a38d49c32c3ff42684d323c78eec79c7
Gustavo Vieira Veloso <gustavovieiraveloso8@gmail.com>
2026-03-27 20:24:34 -0300

    Add files via upload

---
+++ InventarioTI.Teste/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using InventarioTI.Application.Services;
+using InventarioTI.Domain.Entities;
+using InventarioTI.Infrastructure.Repositories;
+
+class Program
+{
+    static void Main()
+    {
+        var repo = new EquipamentoRepository();
+        var service = new EquipamentoService(repo);
+
+        var eq = new Equipamento
+        {
+            Nome = "CAIO ",
+            Tipo = "CADW",
+            Marca = "NIKE",
+            Data_Aquisicao = DateTime.Now
+        };
+
+        service.Adicionar(eq);
+
+        var lista = service.Listar();
+
+        foreach (var item in lista)
+        {
+            Console.WriteLine(item.Nome);
+        }
+
+        if (lista.Count > 0)
+        {
+            var atualizar = lista.First();
+
+            atualizar.Nome = "PC Atualizado";
+
+            service.Atualizar(atualizar);
+            Console.WriteLine("\n Atualizado!");
+        }
+    }
+}
